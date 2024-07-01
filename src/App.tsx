314d18bb6e11cd869650e2685bbfde401cd050a1
Oğuzhan Uysal <law-tekken3@hotmail.com>
2024-07-01 10:50:16 +0300

    Shadcn fixed. Dark theme added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import { Button } from "./shadcn/components/ui/button";
+import { Button } from "./components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
+import { ThemeProvider } from "@/components/theme-provider";
+// import { ModeToggle } from "./components/mode-toggle";
 
 export default function App() {
+  const { toast } = useToast();
   return (
-    <div className="mt-20 flex items-center justify-center">
-      <Button>A Nice Shad button</Button>
-    </div>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <div className="mt-20 flex items-center justify-center">
+        {/* <ModeToggle /> */}
+        <Button
+          onClick={() => {
+            toast({
+              title: "Scheduled: Catch up",
+              description: "Friday, February 10, 2023 at 5:57 PM",
+            });
+          }}
+        >
+          A Nice Shad button
+        </Button>
+      </div>
+    </ThemeProvider>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,13 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "./src/") },
+      {
+        find: "@/components",
+        replacement: path.resolve(__dirname, "./src/components/"),
+      },
+      { find: "@/lib", replacement: path.resolve(__dirname, "./src/lib/") },
+    ],
   },
 });
