beb210fe65d3ebb4180fd96b15b6221c26433ad2
Oğuzhan Uysal <law-tekken3@hotmail.com>
2024-07-01 09:46:37 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./shadcn/components/ui/button";
+
+export default function App() {
+  return (
+    <div className="mt-20 flex items-center justify-center">
+      <Button>A Nice Shad button</Button>
+    </div>
+  );
+}
