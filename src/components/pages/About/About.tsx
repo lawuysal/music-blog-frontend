ed9293df429134be67358f2f9f82b615eb768b48
Oğuzhan Uysal <law-tekken3@hotmail.com>
2024-07-02 11:48:25 +0300

    Completed article page design.
    Also, article page now has a responsive design.

--- src/components/pages/About/About.tsx
+++ src/components/pages/About/About.tsx
@@ -1,3 +1,12 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function About() {
   return (
     <div>
--- src/components/pages/Article/Article.tsx
+++ src/components/pages/Article/Article.tsx
@@ -1,28 +1,32 @@
+import { Separator } from "@/components/ui/separator";
+
 export default function Article() {
   return (
-    <main className="mx-auto grid w-10/12 max-w-screen-xl grid-cols-[7fr_3fr] items-center justify-center justify-items-center">
-      <article className="flex max-w-screen-xl flex-col gap-12">
-        <header className="mt-20 flex max-w-screen-2xl flex-col items-center justify-center gap-10">
+    <main className="mx-auto mb-48 grid w-10/12 max-w-screen-xl grid-cols-1 items-center justify-center justify-items-center">
+      {/* <aside></aside> */}
+      <article className="flex max-w-screen-xl flex-col gap-12 md:w-3/5">
+        <header className="mt-10 flex max-w-screen-2xl flex-col items-center justify-center gap-10 md:mt-20">
           <div className="flex items-center justify-center self-start">
-            <h1 className="text-left text-6xl font-semibold">
+            <h1 className="text-center text-4xl font-semibold md:text-left md:text-6xl">
               Why Music Production Can't Be Simplified
             </h1>
           </div>
-          <div className="flex flex-col items-start justify-center self-start">
+          <div className="flex flex-col items-start justify-center self-start text-sm text-muted-foreground md:text-base">
             <p>Author: Ray Maschine</p>
-            <time dateTime="2024-07-01">Date: July 1, 2024</time>
+            <time dateTime="2024-07-01">Published: July 1, 2024</time>
           </div>
-          <div className="h-[500px] w-max object-cover">
+          <div className="w-full object-cover">
             <img
               src="public\music-article-images\01.jpg"
               alt="music production"
-              className="h-[500px] w-[1000px] rounded-md object-cover"
+              className="h-full w-full rounded-md object-cover ring-1"
             />
           </div>
         </header>
         <section>
-          <h2>Introduction</h2>
-          <p>
+          <h2 className="text-2xl font-bold">Introduction</h2>
+          <Separator className="mb-4" />
+          <p className="text-sm md:text-base">
             Music production is a complex and multifaceted process that involves
             a blend of creativity, technical skills, and artistic vision. While
             technology has made certain aspects more accessible, the intricacies
@@ -30,8 +34,9 @@ export default function Article() {
           </p>
         </section>
         <section>
-          <h2>Main Topic</h2>
-          <p>
+          <h2 className="text-2xl font-bold">Main Topic</h2>
+          <Separator className="mb-4" />
+          <p className="text-sm md:text-base">
             At its core, music production encompasses a variety of stages,
             including composition, recording, mixing, and mastering. Each of
             these stages requires a distinct set of skills and a deep
@@ -42,7 +47,7 @@ export default function Article() {
             mixing and mastering refine the sound to achieve a polished final
             product.
           </p>
-          <p>
+          <p className="text-sm md:text-base">
             Moreover, the subjective nature of music adds another layer of
             complexity. What resonates with one person may not have the same
             effect on another, making it challenging to create universally
@@ -50,8 +55,10 @@ export default function Article() {
             precision with artistic intuition to craft pieces that are both
             technically sound and emotionally engaging.
           </p>
-          <h3>Technological Advances</h3>
-          <p>
+          <br />
+          <h3 className="text-xl font-semibold">Technological Advances</h3>
+          <Separator className="mb-4" />
+          <p className="text-sm md:text-base">
             While advancements in technology have democratized access to music
             production tools, they have also introduced new challenges. Software
             and hardware updates require continuous learning and adaptation.
@@ -61,8 +68,9 @@ export default function Article() {
           </p>
         </section>
         <section>
-          <h2>Conclusion</h2>
-          <p>
+          <h2 className="text-2xl font-bold">Conclusion</h2>
+          <Separator className="mb-4" />
+          <p className="text-sm md:text-base">
             In conclusion, the complexity of music production lies in its
             combination of technical expertise and creative artistry. Despite
             technological advancements making tools more accessible, the nuanced
@@ -72,30 +80,33 @@ export default function Article() {
             unwavering passion for the art form.
           </p>
         </section>
-        <footer>
-          <p>
-            Category: <a href="category-music.html">Music</a>
-          </p>
-          <p>
-            Tags: <a href="tag-production.html">Production</a>,{" "}
-            <a href="tag-music.html">Music</a>
-          </p>
-          <div className="author-bio">
-            <h3>About the Author</h3>
+        <footer className="flex flex-col gap-10">
+          <div className="rounded-md bg-muted p-4 text-sm md:text-base">
+            <p className="">
+              Category: <a href="category-music.html">Music</a>
+            </p>
             <p>
-              John Doe is a music producer with over a decade of experience in
-              the industry. He has worked with numerous artists and enjoys
-              sharing his insights on the creative process.
+              Tags: <a href="tag-production.html">Production</a>,{" "}
+              <a href="tag-music.html">Music</a>
             </p>
           </div>
-          <div className="comments">
-            <h3>Yorumlar</h3>
+          <div className="rounded-md bg-muted p-6">
+            <h3 className="font-semibold">About the Author</h3>
+            <div className="mt-8 flex flex-col items-center justify-center gap-8 md:gap-12 lg:flex-row">
+              <img
+                src="public\author-image.webp"
+                alt="image-of-the-author"
+                className="size-28 rounded-full md:size-32"
+              />
+              <p className="text-sm md:text-base">
+                John Doe is a music producer with over a decade of experience in
+                the industry. He has worked with numerous artists and enjoys
+                sharing his insights on the creative process.
+              </p>
+            </div>
           </div>
         </footer>
       </article>
-      <aside>
-        <h1 className="w-max">Lelloooooooo</h1>
-      </aside>
     </main>
   );
 }
