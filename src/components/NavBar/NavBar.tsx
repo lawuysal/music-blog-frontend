02b6de1711fc2f8412d072ec0376b3307f3d20e0
Oğuzhan Uysal <law-tekken3@hotmail.com>
2024-07-11 03:16:36 +0300

    Updated Navbar
    Navbar navigation menu now centered.

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,16 +6,16 @@ export default function NavBar() {
   const navigate = useNavigate();
   return (
     <header className="mx-auto w-10/12 max-w-screen-xl">
-      <nav className="mt-3 flex flex-col items-center justify-between gap-5 md:mt-5 md:flex-row">
+      <nav className="mt-3 flex flex-col items-center justify-between gap-5 md:mt-5 lg:grid lg:grid-cols-[1fr_2fr_1fr] lg:flex-row lg:justify-items-center">
         <div className="flex h-fit items-center justify-center gap-2">
           <h1
-            className="cursor-pointer text-3xl font-semibold md:text-2xl"
+            className="cursor-pointer text-3xl font-semibold lg:text-2xl"
             onClick={() => navigate("/")}
           >
             Ray's
           </h1>
           <h1
-            className="inline-block cursor-pointer bg-gradient-to-r from-primary to-destructive bg-clip-text text-3xl font-semibold text-transparent md:text-2xl"
+            className="inline-block cursor-pointer bg-gradient-to-r from-primary to-destructive bg-clip-text text-3xl font-semibold text-transparent lg:text-2xl"
             onClick={() => navigate("/")}
           >
             Blog
