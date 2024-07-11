d292900623ea14af1cd1c8bb97b4272794ae8447
Oğuzhan Uysal <law-tekken3@hotmail.com>
2024-07-11 03:19:01 +0300

    Updated About page
    Added background to the about page.

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -15,8 +15,12 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function About() {
   return (
-    <div className="relative flex h-[35rem] items-center justify-center md:mt-0">
-      <Card className="flex w-10/12 max-w-screen-lg flex-col items-center justify-center shadow-black/10 drop-shadow-xl dark:shadow-white/10 md:w-2/5">
+    <div className="relative flex h-[35rem] max-w-6xl items-center justify-center md:mx-auto md:mt-0">
+      <div className="absolute left-[8%] top-16 -z-30 h-[32rem] w-56 -rotate-[30deg] rounded-full bg-primary opacity-40 blur-[72px] filter dark:opacity-30 lg:left-[18%]"></div>
+      <div className="absolute left-[45%] -z-30 h-[30rem] w-28 -rotate-[30deg] rounded-full bg-destructive opacity-50 blur-[72px] filter dark:opacity-45 lg:left-[70%]"></div>
+
+      <div className="absolute left-[35%] top-20 -z-30 h-[20rem] w-28 rotate-[45deg] rounded-full bg-purple-600 opacity-50 blur-[72px] filter dark:opacity-45 lg:left-[50%]"></div>
+      <Card className="flex w-10/12 max-w-screen-lg flex-col items-center justify-center shadow-black/10 drop-shadow-xl dark:shadow-white/10 md:w-[60%]">
         <CardHeader className="mt-8 flex items-center justify-center pb-4">
           <Avatar className="absolute -top-12 aspect-square h-24 w-24 rounded-full object-cover grayscale-[0%]">
             <AvatarImage
